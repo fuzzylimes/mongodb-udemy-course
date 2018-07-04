b8a980d51321b19e74c7f728c3759680f1d936ce
zcochran <fuzzylimes@gmail.com>
2018-07-04 11:26:46 -0400

    Lesson 44 - handling failed inserts

--- users/tests/validation_test.js
+++ users/tests/validation_test.js
@@ -2,6 +2,8 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Validating records', () => {
+    let charLenError = 'Name must be longer than 2 characters.'
+
     it('requires a user name', () => {
         const user = new User({name: undefined, postCount: 5});
         const validationResult = user.validateSync();
@@ -11,6 +13,15 @@ describe('Validating records', () => {
     it('requires a user name longer than 2', () => {
         const user = new User({name: 'Al'});
         const validationResult = user.validateSync();
-        assert(validationResult.errors.name.message === 'Name must be longer than 2 characters.');
+        assert(validationResult.errors.name.message === charLenError);
+    });
+
+    it('disallows invalid records from being saved', (done) => {
+        const user = new User({name: 'Al'});
+        user.save()
+            .catch((validationResult) => {
+                assert(validationResult.errors.name.message === charLenError);
+                done();
+            });
     });
 });
